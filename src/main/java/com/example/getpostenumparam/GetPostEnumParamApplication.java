a3f7cca782d8ba82f41dbc010f66ac586e42d179
Cnfn <cnfnss@gmail.com>
2018-10-27 17:47:42 +0800

    first commit

---
+++ src/main/java/com/example/getpostenumparam/GetPostEnumParamApplication.java
@@ -0,0 +1,12 @@
+package com.example.getpostenumparam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetPostEnumParamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetPostEnumParamApplication.class, args);
+	}
+}
