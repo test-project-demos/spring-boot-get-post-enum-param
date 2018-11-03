b44faca7c71371f02b4cbcfe72f8de07d5c0a560
Cnfn <cnfnss@gmail.com>
2018-11-03 17:15:50 +0800

    refactor: add custom Converter by bean's type

--- src/main/java/com/example/getpostenumparam/config/SpringParamConvertConfig.java
+++ src/main/java/com/example/getpostenumparam/config/SpringParamConvertConfig.java
@@ -1,7 +1,9 @@
 package com.example.getpostenumparam.config;
 
-import com.example.getpostenumparam.spring.deserializer.GenderEnumConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -11,10 +13,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
  */
 @Configuration
 public class SpringParamConvertConfig extends WebMvcConfigurationSupport {
+
+    @Autowired
+    private ApplicationContext context;
+
     @Override
     protected void addFormatters(FormatterRegistry registry) {
         super.addFormatters(registry);
-
-        registry.addConverter(new GenderEnumConverter());
+        context.getBeansOfType(Converter.class).values().forEach(registry::addConverter);
     }
 }
--- src/main/java/com/example/getpostenumparam/spring/deserializer/GenderEnumConverter.java
+++ src/main/java/com/example/getpostenumparam/spring/deserializer/GenderEnumConverter.java
@@ -2,11 +2,13 @@ package com.example.getpostenumparam.spring.deserializer;
 
 import com.example.getpostenumparam.type.GenderEnum;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
 /**
  * @author zhixiao.mzx
  * @date 2018/10/27
  */
+@Component
 public class GenderEnumConverter implements Converter<String, GenderEnum> {
     @Override
     public GenderEnum convert(String value) {
